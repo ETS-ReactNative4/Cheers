060b57605559218181dfbac5fbac5a62e89dddae
nguybr <nguybr@vt.edu>
2022-04-22 01:27:46 -0400

    Server initialization with database connection - must add local mysql database password to server/config/default.json for it to work

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Connect database
+var mysqlPool = require("./mysqlPool");
+mysqlPool.getConnection(function(err, mclient) {
+    let sql = "SELECT user_name FROM users";
+    mclient.query(sql, (err, resp) => {
+        if (err) {
+            throw err;
+        }
+        console.log(resp);
+    })
+});
+
+
+const app = express();
+
+// Init Middleware
+app.use(express.json( { extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));   // makes /api/users pertain to '/' in users.js
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/research', require('./routes/api/researchPosts'));
+// app.use('/api/publication', require('./routes/api/publicationPosts'));
+// app.use('/api/about', require('./routes/api/aboutPosts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
