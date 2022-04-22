0036232e2ce9afb2802991aa8c37ea70e3e02afc
nguybr <nguybr@vt.edu>
2022-04-22 18:25:59 -0400

    Created get endpoints to get all rows for each table

---
+++ server/routes/api/posts.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+var mysqlPool = require("../../mysqlPool");
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+    try {
+    //   const posts = await Post.find().sort({ date: -1 }); // -1 is sort by most recent
+      mysqlPool.getConnection(function(err, mclient) {
+        let sql = `SELECT * FROM posts ORDER BY post_date ASC`;
+        mclient.query(sql, async (err, resp) => {
+            if (err) {
+                throw err;
+            }
+            res.json(resp);
+        })
+    });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+});
+
+module.exports = router;
+
