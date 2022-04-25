3d3ffe2d8479582a8e3e7f1914c33a16af20be81
nguybr <nguybr@vt.edu>
2022-04-25 15:27:03 -0400

    Added edit post functionality

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -83,6 +83,33 @@ router.get("/all", async (req, res) => {
   }
 });
 
+// @route   GET api/posts/:id
+// @desc    Get my post with id
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    mysqlPool.getConnection(function (err, mclient) {
+      let sql = `SELECT posts.post_id, posts.star_num, posts.post_date, posts.title, posts.user_name, 
+      recipes.recipe_id, recipes.instructions, drink_category.category_name, drink_category.id
+      FROM posts,recipes,drink_category
+      WHERE posts.post_id = recipes.post_id
+      AND drink_category.id = recipes.category_id
+      AND posts.post_id = "${req.params.id}"
+      ORDER BY post_date ASC`;
+      mclient.query(sql, async (err, resp) => {
+        if (err) {
+          throw err;
+        }
+        mclient.release();
+        res.json(resp[0]);
+      });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 // @route   POST api/posts
 // @desc    Create a recipe post
 // @access  Private
@@ -221,6 +248,7 @@ router.post(
                                         "Transaction Completed Successfully."
                                       );
                                       connection.end();
+                                      res.json(result);
                                     });
                                   }
                                 }
@@ -261,6 +289,7 @@ router.post(
                                   "Transaction Completed Successfully."
                                 );
                                 connection.end();
+                                res.json(result);
                               });
                             }
                           }
@@ -269,50 +298,218 @@ router.post(
                     }
                   );
                 });
-                // connection.query(
-                //   "INSERT INTO ingredients(ingredient_name) VALUES ?",
-                //   [ingredients],
-                //   function (err, result) {
-                //     if (err) {
-                //       // connection.rollback(function () {
-                //       //   throw err;
-                //       // });
-                //     }
-                //     // Create objects to insert into uses
-                //     let firstInsert = result.insertId;
-                //     let uses = [];
-                //     for (let i = 0; i < ingredients.length; i++) {
-                //       let innerArray = [recipeId, firstInsert + i];
-                //       uses.push(innerArray);
-                //     }
-                //     connection.query(
-                //       "INSERT IGNORE INTO uses(recipe_id, ingredient_id) VALUES ?",
-                //       [uses],
-                //       function (err, result) {
-                //         if (err) {
-                //           connection.rollback(function () {
-                //             throw err;
-                //           });
-                //         }
-                //         connection.commit(function (err) {
-                //           if (err) {
-                //             connection.rollback(function () {
-                //               throw err;
-                //             });
-                //           }
-                //           console.log("Transaction Completed Successfully.");
-                //           connection.end();
-                //         });
-                //       }
-                //     );
-                //   }
-                // );
               }
             );
           }
         );
       });
-      /* End transaction */
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   PUT api/posts/:id
+// @desc    Edit post with id
+// @access  Private
+router.put(
+  "/:id",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("instructions", "Instructions are required").not().isEmpty(),
+      check("category", "Category is required").not().isEmpty(),
+      check("star_num", "Rating is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let {
+      title,
+      instructions,
+      category,
+      star_num,
+      ingredients,
+      post_id,
+      recipe_id,
+    } = req.body;
+
+    try {
+      let editedPost = {
+        star_num,
+        title,
+      };
+
+      const connection = mysql.createConnection(config.get("mysql"));
+      connection.connect(function (err) {
+        if (err) {
+          console.error("error connecting: " + err.stack);
+          return;
+        }
+      });
+
+      /* Begin transaction */
+      connection.beginTransaction(function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("Beginning of UPDATE Transaction");
+
+        connection.query(
+          `UPDATE posts SET ? WHERE post_id = ?`,
+          [editedPost, req.params.id],
+          async function (err, result) {
+            if (err) {
+              connection.rollback(function () {
+                throw err;
+              });
+            }
+            console.log("updated post OK");
+
+            let editedRecipe = {
+              instructions,
+              category_id: category,
+            };
+            connection.query(
+              `UPDATE recipes SET ? WHERE recipe_id = ?`,
+              [editedRecipe, recipe_id],
+              async function (err, result) {
+                if (err) {
+                  connection.rollback(function () {
+                    throw err;
+                  });
+                }
+                console.log("updated recipe  OK");
+
+                // Delete all uses for this recipe and then re-insert
+                connection.query(
+                  `DELETE FROM uses WHERE recipe_id = "${recipe_id}"`,
+                  async function (err, result) {
+                    if (err) {
+                      console.log(err);
+                      connection.rollback(function () {
+                        throw err;
+                      });
+                    }
+                    console.log("deleted uses OK");
+                    // insert ingredients one at a time; at each successful iteration, get insertId and add to list of uses{recipeId, ingredient_id} to query later
+                    // If not successful, get ingredient id using ingredient name and add to list of uses{recipeId, ingredient_id} to query
+                    let uses = [];
+                    ingredients.forEach((ingredient, index) => {
+                      // console.log([[ingredient]])
+                      connection.query(
+                        "INSERT INTO ingredients(ingredient_name) VALUES ?",
+                        [[ingredient]],
+                        function (err, result) {
+                          if (err) {
+                            if (err.code === "ER_DUP_ENTRY") {
+                              // Duplicate entry
+                              console.log(
+                                "DUPLICATE ENTRY-finding existing ingredient id"
+                              );
+                              connection.query(
+                                `SELECT ingredients.id from ingredients WHERE ingredients.ingredient_name = "${ingredient[0]}"`,
+                                function (err, result) {
+                                  if (err) {
+                                    console.log("ERROR");
+                                    connection.rollback(function () {
+                                      throw err;
+                                    });
+                                  }
+                                  uses.push([recipe_id, result[0].id]);
+                                  console.log(uses);
+                                  // Insert into uses
+                                  connection.query(
+                                    "INSERT INTO uses SET ?",
+                                    {
+                                      recipe_id: recipe_id,
+                                      ingredient_id: result[0].id,
+                                    },
+                                    function (err, result) {
+                                      if (err) {
+                                        connection.rollback(function () {
+                                          throw err;
+                                        });
+                                      }
+                                      console.log(
+                                        "successful insert into uses"
+                                      );
+                                      if (index === ingredients.length - 1) {
+                                        connection.commit(function (err) {
+                                          if (err) {
+                                            connection.rollback(function () {
+                                              throw err;
+                                            });
+                                          }
+                                          console.log(
+                                            "Transaction Completed Successfully."
+                                          );
+                                          connection.end();
+                                          res.json(result);
+                                        });
+                                      }
+                                    }
+                                  );
+                                }
+                              );
+                            } else {
+                              connection.rollback(function () {
+                                throw err;
+                              });
+                            }
+                          } else {
+                            console.log(
+                              "new ingredient - getting last insert id"
+                            );
+                            let lastInsertId = result.insertId;
+                            uses.push([recipe_id, lastInsertId]);
+                            console.log(uses);
+                            connection.query(
+                              "INSERT INTO uses SET ?",
+                              {
+                                recipe_id: recipe_id,
+                                ingredient_id: lastInsertId,
+                              },
+                              function (err, result) {
+                                if (err) {
+                                  connection.rollback(function () {
+                                    throw err;
+                                  });
+                                }
+                                console.log("successful insert into uses");
+                                if (index === ingredients.length - 1) {
+                                  connection.commit(function (err) {
+                                    if (err) {
+                                      connection.rollback(function () {
+                                        throw err;
+                                      });
+                                    }
+                                    console.log(
+                                      "Transaction Completed Successfully."
+                                    );
+                                    connection.end();
+                                    res.json(result);
+                                  });
+                                }
+                              }
+                            );
+                          }
+                        }
+                      );
+                    });
+                  }
+                );
+              }
+            );
+          }
+        );
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
